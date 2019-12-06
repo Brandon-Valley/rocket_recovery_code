dd0832e3cf29b2a8749b2f88d7f5353af9855fb5
Brandon Valley <bavvh8@mst.edu>
2019-12-06 00:18:27 -0600

    ready for vid

--- gps_test/gps_test.ino
+++ gps_test/gps_test.ino
@@ -7,5 +7,18 @@
 // Connect GPS RX (data into GPS) to Digital 0
 // Connect GPS TX (data out from GPS) to Digital 1
  
-void setup() {}
-void loop() {}
+void setup() 
+{
+   Serial.begin(9600);
+   Serial.println("<Arduino is ready>");  
+}
+void loop() 
+{
+  delay(1500);  
+  //Serial.print(random(37.954077,50));
+  Serial.print(37.95);
+  Serial.print(random(4077,5088));
+  Serial.print(" , ");
+  Serial.print(-91.76);
+  Serial.println(random(7779,9130));
+  }
